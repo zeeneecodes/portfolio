87cfdef5d899a936992cefe90c68e216463a3ac5
Rizzi <cabrerarizzi@gmail.com>
2025-11-28 15:11:25 +0800

    fix: add tag render key, update 2nd button to neutral

--- src/ui/Card.jsx
+++ src/ui/Card.jsx
@@ -5,17 +5,29 @@ function Card({ tags, title, description, liveLink, studyLink, image, alt }) {
     <div className="card">
       <img src={image} alt={alt} />
       <div className="flex w-[254px] gap-1 flex-wrap">
-        {tags.map((tag) => (
-          <Tag tag={tag} />
+        {tags.map((tag, i) => (
+          <Tag key={i} tag={tag} />
         ))}
       </div>
       <h3>{title}</h3>
       <p>{description}</p>
 
       {liveLink || studyLink ? (
-        <div className="flex mt-6 justify-around">
-          {liveLink && <button className="btn-primary">Live Demo</button>}
-          {studyLink && <button className="btn-primary">Read More</button>}
+        <div className="flex mt-6 justify-between">
+          {liveLink && (
+            <button className="btn-primary">
+              <a href={liveLink} target="_blank" rel="noopener noreferrer">
+                Live Demo
+              </a>
+            </button>
+          )}
+          {studyLink && (
+            <button className="btn-neutral">
+              <a href={liveLink} target="_blank" rel="noopener noreferrer">
+                Read More
+              </a>
+            </button>
+          )}
         </div>
       ) : null}
     </div>

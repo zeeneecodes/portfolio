f79fb02cd001f3c3c84f28c74cc8d7efed34e981
Rizzi <cabrerarizzi@gmail.com>
2025-12-12 17:38:18 +0800

    hero component

---
+++ src/features/personal/Hero.jsx
@@ -0,0 +1,18 @@
+import { FaEnvelope, FaGithub, FaLinkedin } from "react-icons/fa";
+import Icon from "../../ui/Icon";
+
+function Hero() {
+  return (
+    <header className="bg-[url(header.jpg)] bg-bottom-right bg-contain bg-no-repeat lg:bg-cover lg:bg-center h-dvh flex items-center md:pl-[10%] max-md:justify-center">
+      <div>
+        <h1 className="text-5xl md:text-7xl lg:text-8xl">Hi, I'm Rizzi!</h1>
+        <p className="mb-3">Some text and personal intro</p>
+        <Icon type="regular" icon={<FaEnvelope />} title="Email" />
+        <Icon type="regular" icon={<FaLinkedin />} title="LinkedIn" />
+        <Icon type="regular" icon={<FaGithub />} title="Github" />
+      </div>
+    </header>
+  );
+}
+
+export default Hero;

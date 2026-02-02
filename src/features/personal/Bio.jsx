79b9a6c04e2971c69d577b72c5eaa5e9d1be33dd
Rizzi <cabrerarizzi@gmail.com>
2026-02-02 16:08:54 +0800

    Bio.jsx fix container stretch

--- src/features/personal/Bio.jsx
+++ src/features/personal/Bio.jsx
@@ -1,25 +1,36 @@
 import { FaEnvelope, FaGithub, FaLinkedin } from "react-icons/fa";
 import Icon from "../../ui/Icon";
 import Timeline from "./Timeline";
+import Content from "../../ui/Content";
 
 function Bio() {
   return (
-    <section className="w-full">
-      <section>
-        <h1>Hi, I'm Rizzi Cabrera!</h1>
-        <div>
-          <Icon type="regular" icon={<FaEnvelope />} title="Email" />
-          <Icon type="regular" icon={<FaLinkedin />} title="LinkedIn" />
-          <Icon type="regular" icon={<FaGithub />} title="Github" />
-        </div>
-      </section>
-      <section className="my-20">
+    <div className="flex flex-col justify-center items-center pt-10">
+      <div className="max-w-5xl px-6 gap-20 items-stretch">
+        <section>
+          <h1>Hi, I'm Rizzi Cabrera!</h1>
+          <div>
+            <Icon type="regular" icon={<FaEnvelope />} title="Email" />
+            <Icon type="regular" icon={<FaLinkedin />} title="LinkedIn" />
+            <Icon type="regular" icon={<FaGithub />} title="Github" />
+          </div>
+        </section>
+        <section>
+          <Content>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat,
+              sed totam nam in, nostrum distinctio fugiat facilis minima optio
+              modi temporibus unde, vitae iure quo nobis. Corporis minima
+              possimus consectetur.
+            </p>
+          </Content>
+        </section>
         <Timeline />
-      </section>
 
-      {/* sent */}
-      <section></section>
-    </section>
+        {/* sent */}
+        <section></section>
+      </div>
+    </div>
   );
 }
 

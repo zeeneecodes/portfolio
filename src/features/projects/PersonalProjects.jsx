fc8af1d29f05c5526c7a77bafb6cf1f4bac149d9
Rizzi <cabrerarizzi@gmail.com>
2026-02-07 19:17:21 +0800

    move past projects section to PersonalProjects.jsx

--- src/features/projects/PersonalProjects.jsx
+++ src/features/projects/PersonalProjects.jsx
@@ -8,41 +8,8 @@ function PersonalProjects() {
   return (
     <section className="w-full">
       {/* BIG FINISHED PROJECTS */}
-      <section className="mb-36">
+      <section>
         <h1 className="mb-15">Featured Projects</h1>
-        {/* <ProjectPreview
-          img="https://upload-os-bbs.hoyolab.com/upload/2024/12/16/19612479/57d5ece48080fc485856bcabc24158f4_2511853629224699986.jpeg"
-          title="Money Pouch"
-          tags={[
-            "React",
-            "JavaScript",
-            "React Router",
-            "Tanstack Query",
-            "Supabase",
-            "Sass",
-            "React Hook Form",
-          ]}
-          description=""
-          liveLink=""
-          studyLink=""
-        />
-        <ProjectPreview
-          img="https://upload-os-bbs.hoyolab.com/upload/2024/12/16/19612479/57d5ece48080fc485856bcabc24158f4_2511853629224699986.jpeg"
-          title="Portfolio"
-          tags={["React", "Tailwind"]}
-          description="The Scepter δ-me13, a creation of Rubert II, is an astral computer that the simulated world of Amphoreus runs on in continuous cycles. Unlike other Scepters, δ-me13 once served as a Celestial-Body Neuron for Nous, but was later discarded."
-          liveLink=""
-          studyLink=""
-        />
-        
-        <ProjectPreview
-          img="https://upload-os-bbs.hoyolab.com/upload/2024/12/16/19612479/57d5ece48080fc485856bcabc24158f4_2511853629224699986.jpeg"
-          title="CAWPILE Rating Calculator"
-          tags={["React", "Tailwind"]}
-          description="The Scepter δ-me13, a creation of Rubert II, is an astral computer that the simulated world of Amphoreus runs on in continuous cycles. Unlike other Scepters, δ-me13 once served as a Celestial-Body Neuron for Nous, but was later discarded."
-          liveLink=""
-          studyLink=""
-        /> */}
         <ProjectPreview
           title="Money Pouch"
           tags={[
@@ -88,25 +55,112 @@ function PersonalProjects() {
         />
       </section>
 
-      {/* PAST PROJECTS */}
-      <section>
-        <h1>My Other Works</h1>
-        <div className="flex gap-4 mt-4 mb-40">
-          <StudyProjects />
-        </div>
-      </section>
-
       {/* IN PROGRESS PROJECT/S */}
-      <section className="mb-10">
-        <div className="flex gap-1 flex-wrap">
-          <Tag tag="in  progress" />
-        </div>
-        <Content heading="h2" title="Note App">
-          <p>
-            I am currently working on a note webapp that has a Rich Text Format
-            (RTF) functionality
+      <section className="my-20">
+        <Content heading="h2" title="CAWPILE Calculator">
+          <div className="flex gap-1 flex-wrap mt-3">
+            <Tag tag="in  progress" />
+          </div>
+          <p className="mt-5">
+            A web app calculator based on a book rating system created by Book
+            Roast.
           </p>
         </Content>
+      </section>
+
+      {/* <StudyProjects /> */}
+      <section>
+        <h1>My Other Works</h1>
+        <p className="mb-15">
+          These are the small/foundational knowledge projects that I have worked
+          on!
+        </p>
+
+        <ProjectPreview
+          title="Bitendo"
+          tags={["Advanced JavaScript", "DOM Manipulation", "Sass"]}
+          description="A parody imitation of Nintendo Switch's store page that toes the line between a static and a dynamic app. This is a practice project that is focused on exploring how Advanced JavaScript and DOM manipulation can really bring a web app to life. The carousel, like button, cards, and accordions are all interactive."
+          img={[
+            {
+              pic: "/bitendo-xs.jpg",
+              media: "(max-width:640px)",
+            },
+            {
+              pic: "/bitendo-md.jpg",
+              media: "(max-width:768px)",
+            },
+          ]}
+          liveLink="https://bitendo-product-page.vercel.app/"
+          alt='Picture of Bitendo Screenshot with on a green background and an additional context subtitle of "Interactive Static Page"'
+        />
+        <ProjectPreview
+          title="Excuse Generator"
+          tags={["React", "JavaScript", "State Management"]}
+          description="An applied learning project built after studying basic JavaScript API calls and foundational React concepts, including reusable components, useState, and useEffect."
+          img={[
+            {
+              pic: "/excuse-xs.jpg",
+              media: "(max-width:640px)",
+            },
+            {
+              pic: "/excuse-md.jpg",
+              media: "(max-width:768px)",
+            },
+          ]}
+          liveLink="https://zeeneecodes.github.io/excuse-generator/"
+          alt='Picture with a green background containing screenshots of the Excuse Generator webapp. Additional context subtitle of "Frontend Project with External API"'
+        />
+        <ProjectPreview
+          title="Flower"
+          tags={["HTML", "Sass", "JavaScript"]}
+          description="A learning exercise on how javascript can make an html element interactive. This is also a hands-on practice on responsive websites."
+          img={[
+            {
+              pic: "/flower-xs.jpg",
+              media: "(max-width:640px)",
+            },
+            {
+              pic: "/flower-md.jpg",
+              media: "(max-width:768px)",
+            },
+          ]}
+          liveLink="https://zeeneecodes.github.io/pricing-page/"
+          alt='Picture of screenshots of the Flower website with on a green background and an additional context subtitle of "Interactive Static Page"'
+        />
+        <ProjectPreview
+          title="Preppy"
+          tags={["HTML", "Sass", "Responsive Design"]}
+          description="A purely static website that also doubles as a practice on responsive websites."
+          img={[
+            {
+              pic: "/preppy-xs.jpg",
+              media: "(max-width:640px)",
+            },
+            {
+              pic: "/preppy-md.jpg",
+              media: "(max-width:768px)",
+            },
+          ]}
+          liveLink="https://zeeneecodes.github.io/practice-faq-landing/"
+          alt='Picture of two screenshots of the Preppy website with on a green background and an additional context subtitle of "Interactive Static Page"'
+        />
+        <ProjectPreview
+          title="YouTube Clone"
+          tags={["HTML", "CSS", "Responsive Design", "JavaScript"]}
+          description="After completing the final project of Supersimpledev's HTML & CSS course, this was my attempt at challenging myself and picking off from the project's base static landing YouTube page. I added all the missing features and interactivity that I wanted to see by only using HTML, vanilla CSS, and vanilla JavaScript. My continuation is based on YouTube's 2024 UI/UX."
+          img={[
+            {
+              pic: "/ytclone-xs.jpg",
+              media: "(max-width:640px)",
+            },
+            {
+              pic: "/ytclone-md.jpg",
+              media: "(max-width:768px)",
+            },
+          ]}
+          liveLink="https://zeeneecodes.github.io/youtube-clone/"
+          alt='Picture of two screenshots of the YouTube Clone website with on a green background and an additional context subtitle of "Interactive Static Page"'
+        />
       </section>
     </section>
   );

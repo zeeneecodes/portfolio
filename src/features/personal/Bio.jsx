c6ebaecd93f499334fa1d9119cc2e765d68381fa
Rizzi <cabrerarizzi@gmail.com>
2026-02-10 19:37:35 +0800

    add content for Hero and About

--- src/features/personal/Bio.jsx
+++ src/features/personal/Bio.jsx
@@ -7,9 +7,9 @@ function Bio() {
   return (
     <div className="flex flex-col justify-center items-center pt-10">
       <div className="max-w-5xl px-6 gap-20 items-stretch">
-        <section>
+        <section className="mb-10">
           <h1>Hi, I'm Rizzi Cabrera!</h1>
-          <div>
+          <div className="mt-3">
             <Icon type="regular" icon={<FaEnvelope />} title="Email" />
             <Icon type="regular" icon={<FaLinkedin />} title="LinkedIn" />
             <Icon type="regular" icon={<FaGithub />} title="Github" />
@@ -18,10 +18,27 @@ function Bio() {
         <section>
           <Content>
             <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat,
-              sed totam nam in, nostrum distinctio fugiat facilis minima optio
-              modi temporibus unde, vitae iure quo nobis. Corporis minima
-              possimus consectetur.
+              I'm a frontend dev that was introduced to the world of HTML and
+              CSS back in 6th grade, it was an extremely bare bones type of
+              introduction but ever since that moment, I've held a fascination
+              towards anything relating to websites and webapps.
+            </p>
+            <p>
+              My journey started with limited access to internet tools. In
+              college, I also didn't have proper opportunities to formally study
+              web development. I found myself exploring roles in back-office
+              support and database administration instead.
+            </p>
+            <p>
+              Still, I kept looking for that same thrill I felt when I launched
+              my very first “Hello World” that was glowing against a bright neon
+              background, on a browser at my school's computer lab.
+            </p>
+            <p>
+              It has been a long and solo journey but through perseverance, and
+              powered by my pure drive of curiosity, I managed to teach myself
+              the proper foundations on HTML, CSS, and JavaScript. Eventually I
+              dipped my toes in React, Tailwind, and Sass.
             </p>
           </Content>
         </section>
--- src/features/personal/Hero.jsx
+++ src/features/personal/Hero.jsx
@@ -3,28 +3,34 @@ import Icon from "../../ui/Icon";
 
 function Hero() {
   return (
-    <header className="bg-[url(header.jpg)] md:bg-[url(header-md.jpg)] lg:bg-[url(header-lg.jpg)] bg-bottom-right bg-contain bg-no-repeat lg:bg-cover lg:bg-center h-dvh flex items-center md:pl-[10%] max-md:justify-center">
-      <div>
+    <header className="bg-[url(header.jpg)] md:bg-[url(header-md.jpg)] lg:bg-[url(header-lg.jpg)] bg-bottom-right bg-contain bg-no-repeat lg:bg-cover lg:bg-center h-dvh flex items-start lg:items-center md:pl-[10%] max-md:justify-center">
+      <div className="mt-60 md:mt-30 lg:mt-0 mx-5">
         <h1 className="text-5xl md:text-7xl lg:text-8xl">Hi, I'm Rizzi!</h1>
-        <p className="mb-3">Some text and personal intro</p>
-        <Icon
-          type="regular"
-          icon={<FaEnvelope />}
-          title="Email"
-          link="mailto:cabrerarizzi@gmail.com"
-        />
-        <Icon
-          type="regular"
-          icon={<FaLinkedin />}
-          title="LinkedIn"
-          link="https://www.linkedin.com/in/rizzi-eunice-cabrera-54a687148/"
-        />
-        <Icon
-          type="regular"
-          icon={<FaGithub />}
-          title="Github"
-          link="https://github.com/zeeneecodes"
-        />
+        <p>
+          Endless curiosity and learning is what drives me. I love single player
+          games and reading books.
+        </p>
+        <p>📖 Currently reading: The Stormlight Archive</p>
+        <div className="mt-5">
+          <Icon
+            type="regular"
+            icon={<FaEnvelope />}
+            title="Email"
+            link="mailto:cabrerarizzi@gmail.com"
+          />
+          <Icon
+            type="regular"
+            icon={<FaLinkedin />}
+            title="LinkedIn"
+            link="https://www.linkedin.com/in/rizzi-eunice-cabrera-54a687148/"
+          />
+          <Icon
+            type="regular"
+            icon={<FaGithub />}
+            title="Github"
+            link="https://github.com/zeeneecodes"
+          />
+        </div>
       </div>
     </header>
   );

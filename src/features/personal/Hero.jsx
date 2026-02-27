81d2981a30ddc2cab81e99293e1a5d7e99bb49e4
Rizzi <cabrerarizzi@gmail.com>
2026-02-27 15:17:15 +0800

    hero: include frontend dev title

--- src/features/personal/Hero.jsx
+++ src/features/personal/Hero.jsx
@@ -4,14 +4,17 @@ import Icon from "../../ui/Icon";
 function Hero() {
   return (
     <header className="bg-[url(/header.jpg)] md:bg-[url(/header-md.jpg)] lg:bg-[url(/header-lg.jpg)] bg-bottom-right bg-contain bg-no-repeat lg:bg-cover lg:bg-center h-dvh flex items-start lg:items-center md:pl-[10%] max-md:justify-center">
-      <div className="mt-60 md:mt-30 lg:mt-0 mx-5">
-        <h1 className="text-5xl md:text-7xl lg:text-8xl">Hi, I'm Rizzi!</h1>
+      <div className="mt-60 md:mt-30 lg:mt-0 mx-5 max-w-10/12">
+        <h1 className="text-5xl md:text-7xl lg:text-8xl text-text">
+          Hi, I'm Rizzi!
+        </h1>
+        <h2>Frontend Developer</h2>
         <p>
           Endless curiosity and learning is what drives me. I love single player
           games and reading books.
         </p>
         <p>📖 Currently reading: The Stormlight Archive</p>
-        <div className="mt-5">
+        <div className="mt-7">
           <Icon
             type="regular"
             icon={<FaEnvelope />}
